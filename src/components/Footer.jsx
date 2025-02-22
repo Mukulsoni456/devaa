50a234eaa64d887931347e7734d6b5351752bb0b
Mukul Soni <mukulsoni@Mukuls-MacBook-Air.local>
2025-02-22 18:38:17 +0530

    Updated footer with Admin Login & Signup links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,31 @@
 import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, logout } from "../firebaseConfig";
 
 export default function Footer() {
+  const [user] = useAuthState(auth); // Get the logged-in user
+
   return (
     <footer className="bg-gray-900 text-white py-20">
       <div className="flex justify-center space-x-6 mb-6">
-        <Link to="/admin-login" className="text-gray-400 hover:text-white">
-          Admin Login
-        </Link>
-        <Link to="/admin-signup" className="text-gray-400 hover:text-white">
-          Admin Signup
-        </Link>
+        {!user ? (
+          <>
+            <Link to="/admin-login" className="text-gray-400 hover:text-white">
+              Admin Login
+            </Link>
+            <Link to="/admin-signup" className="text-gray-400 hover:text-white">
+              Admin Signup
+            </Link>
+          </>
+        ) : (
+          <button 
+            onClick={logout} 
+            className="text-gray-400 hover:text-white cursor-pointer"
+          >
+            Logout
+          </button>
+        )}
       </div>
 
       {/* Copyright */}
